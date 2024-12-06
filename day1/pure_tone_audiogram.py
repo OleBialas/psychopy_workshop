88238c1c8920e434207f903de2d4db77b27832b0
ole <ole.bialas@posteo.de>
2024-12-06 11:15:33 +0100

    liveshare backup

--- day1/pure_tone_audiogram.py
+++ day1/pure_tone_audiogram.py
@@ -3,35 +3,40 @@ import numpy as np
 from psychopy import core, sound
 from psychopy.hardware import keyboard
 
-def audiogram(frequency, dur=0.25, start_intensity=1, step=0.1, n_reversals=5):
+###  define param
 
-    kb = keyboard.Keyboard()
-    tone = sound.Sound(value=frequency, stereo=False)
-    noise = sound.Sound(stereo=False)
-    noise.setSoundArr(np.random.randn(48000))
-    noise.play(loop=-1)
-    reversals=[]
-    direction=-1
-    while len(reversals) < n_reversals:
-        core.wait(np.random.randint(500, 2000)/1000)
-        tone.play()
-        keys = kb.waitKeys(keyList=['space'], maxWait=1)
-        if len(keys) == 0:
-            ... # increase level
-            if direction == -1:
-                direction = 1
-                reversals.append() # append current level
-        else:
-            ... #decrease level
-            if direction == 1:
-                direction = -1
-                reversals.append() # append current level
-    print(f"The detection threshold for {frequency} Hz is {np.mean(reversals)}")
+### pasrse command line arguments ###
+parser = argparse.ArgumentParser()
+parser.add_argument("frequency", type=int, help="Frequency of the tone in Hz")
+parser = argparse.ArgumentParser(description="Run a pure tone audiogram for a single frequency")
+args = parser.parse_args()
+
+kb = keyboard.Keyboard(tone = sound.Sound(value=args.frequency, stereo=False)
+noise = sound.Sound(stereo=False)
+noise.setSoundArr(np.random.randn(48000)))
+
+tone = sound.Sound(value=args.frequency, stereo=False)
+noise = sound.Sound(stereo=False)
+noise.setSoundArr(np.random.randn(48000))
+
+reversals=[]
+direction=-1  # directon of the staircae
+noise.play(loop=-1)
+while len(reversals) < n_reversals:
+    core.wait(np.random.randint(500, 2000)/1000)
+    tone.play()
+    keys = kb.waitKeys(keyList=['space'], maxWait=1)
+    if len(keys) == 0:
+        ... # increase level
+        if direction == -1:
+            direction = 1
+            reversals.append() # append current level
+    else:
+        ... #decrease level
+        if direction == 1:
+            direction = -1
+            reversals.append() # append current level
+print(f"The detection threshold for {frequency} Hz is {np.mean(reversals)}")
         
-if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Run a pure tone audiogram for a single frequency")
-    parser.add_argument("frequency", type=int, help="Frequency of the tone in Hz")
-    args = parser.parse_args()
-    audiogram(args["frequency"])
 

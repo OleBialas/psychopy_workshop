1ebb66434c1635d1e60fef99b910d3016979a608
Ole Bialas <ole.bialas@posteo.de>
2024-12-11 20:44:22 +0100

    all exercises for 5 work

--- 05_functions/make_tone.py
+++ 05_functions/make_tone.py
@@ -2,3 +2,9 @@ from psychopy.sound import Sound
 
 def make_tone(frequency=300, duration=0.3):
     return Sound(value=frequency, secs=duration)
+
+def make_and_play_tone(frequency=300, duration=0.3, play=False):
+    tone = Sound(value=frequency, secs=duration)
+    if play:
+        tone.play()
+    return tone
--- 05_functions/my_functions.py
+++ 05_functions/my_functions.py
@@ -1,13 +1,40 @@
-from typing import Dict
+import random
 
-def get_stimulus_properties(stim_id: str) -> Dict:
-    """Get stimulus duration and position."""
-    return {
-        "duration": 0.5,
-        "position_x": 0.0,
-        "position_y": 0.0
-    }
+# Solution
+def say_hi_to(first:str, last:str="", do_print:bool=False)->str:
+    text = "Hi" + " " + first + " " + last + "!"
+    if do_print:
+        print(text)
+    else:
+        return text
+    
 
-def present_stimulus(stim_id: str) -> None:
-    props = get_stimulus_properties(stim_id)
-    duration = props["duraton"]  # Typo in key!
+# Solution
+def shuffle_trials(trials:list, max_iter:int=1000) -> list:
+    ok = False
+    count = 0
+    while not ok:
+        count+=1
+        if count > max_iter:
+            raise StopIteration
+        found_duplicate = False
+        for i in range(1, len(trials)):
+            if trials[i] == trials[i-1]:
+                found_duplicate = True
+        if not found_duplicate:
+            ok = True
+        else:
+            random.shuffle(trials)
+    return trials
+
+# Solution
+def find_primes(start:int, stop:int) -> list:
+    primes=[]
+    for i in range(start, stop):
+        is_prime = True
+        for j in range(2,int(i/2)):
+            if i%j == 0:
+                is_prime = False
+        if is_prime:
+            primes.append(i)
+    return primes
